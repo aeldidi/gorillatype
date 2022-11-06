98aa94eaca0ea5acdef3e6af19bd288bdf222806
Ayman El Didi <ayman@eldidi.org>
2022-11-06 16:47:25 -0700

    added more texts to choose from

--- texts.js
+++ texts.js
@@ -7,20 +7,46 @@ function words(text) {
 // list of the words to type.
 export default [
     {
-        from: 'Ayman\'s brain',
-        text: words('I\'m a monkey. Ooh ooh ah ah.')
+        from: 'Dracula by Bram Stoker',
+        text: words(`
+        Oh, the terrible struggle that I
+        have had against sleep so often of late; the pain of the sleeplessness,
+        or the pain of the fear of sleep, with such unknown horrors as it has
+        for me! How blessed are some people, whose lives have no fears, no
+        dreads; to whom sleep is a blessing that comes nightly, and brings
+        nothing but sweet dreams.
+        `)
+    },
+    {
+        from: 'The Great Gatsby by F. Scott Fitzgerald',
+        text: words(`
+        "Can't repeat the past?" he cried incredulously. "Why of course you
+        can!"
+
+        He looked around him wildly, as if the past were lurking here in the
+        shadow of his house, just out of reach of his hand.
+        `)
+    },
+    {
+        from: 'Frankenstein; Or, The Modern Prometheus by Mary Wollstonecraft Shelley',
+        text: words(`
+        This state of mind preyed upon my health, which had perhaps never
+        entirely recovered from the first shock it had sustained. I shunned the
+        face of man; all sound of joy or complacency was torture to me;
+        solitude was my only consolation - deep, dark, deathlike solitude.        
+        `)
     },
     {
         from: 'Moby Dick; Or, The Whale by Herman Melville',
         text: words(`
-            Whenever I find myself growing grim about the mouth; whenever it is
-            a damp, drizzly November in my soul; whenever I find myself
-            involuntarily pausing before coffin warehouses, and bringing up the
-            rear of every funeral I meet; and especially whenever my hypos get
-            such an upper hand of me, that it requires a strong moral principle
-            to prevent me from deliberately stepping into the street, and
-            methodically knocking people's hats off - then, I account it high
-            time to get to sea as soon as I can.
+        Whenever I find myself growing grim about the mouth; whenever it is a
+        damp, drizzly November in my soul; whenever I find myself involuntarily
+        pausing before coffin warehouses, and bringing up the rear of every
+        funeral I meet; and especially whenever my hypos get such an upper hand
+        of me, that it requires a strong moral principle to prevent me from
+        deliberately stepping into the street, and methodically knocking
+        people's hats off - then, I account it high time to get to sea as soon
+        as I can.
         `)
     }
 ]
